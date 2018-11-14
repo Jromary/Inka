85a5ca5dd5a3493b3a8c9fbf5776af30ce5f56a2
Jromary <julienromary@yahoo.fr>
2018-11-14 11:10:41 +0100

    ajout de la classe lesClient

---
+++ a1/lesclients.cpp
@@ -0,0 +1,5 @@
+#include "lesclients.h"
+
+LesClients::LesClients()
+{
+}
---
+++ a1/lesclients.hpp
@@ -0,0 +1,10 @@
+#ifndef LESCLIENTS_H
+#define LESCLIENTS_H
+
+class LesClients
+{
+public:
+    LesClients();
+};
+
+#endif // LESCLIENTS_H
